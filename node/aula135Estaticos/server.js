10392d518b3e5d91a852feec4c10f389c43d298a
Rodrigo Prieto <rcprieto@gmail.com>
2021-12-28 11:13:10 -0300

    ok

---
+++ node/aula135Estaticos/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const path = require("path");
+
+//Para passar param no bady
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(routes);
+
+app.listen(3200, () => {
+  console.log("Server Running on port 3200.", "Acessar http://localhost:3200");
+});
+
+//pelo terminal, acessar a pasta e rodar node server.js
+//npm i nodemon --save-dev
+//npx nodemon server.js (ou npm start)
