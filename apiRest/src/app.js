33522d1a73c3486a7121c071abc94fc0f83bcafb
Rodrigo Prieto <rcprieto@gmail.com>
2022-06-29 13:06:09 -0300

    Ima

--- apiRest/src/app.js
+++ apiRest/src/app.js
@@ -26,7 +26,7 @@ class App {
       extended: true,
     }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, 'uploads')));
+    this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
